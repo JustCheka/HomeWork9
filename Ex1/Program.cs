c633a8d454fc01eb73b1b57fb0fb1843d6ecf1f2
JustCheka <cheka201@yandxe.ru>
2023-08-27 22:07:54 +0300

    Ex 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿void ShowNum(int m, int n){
+    if (m >= n) {
+        return ;
+    }
+
+    if (m % 2 == 0) Console.Write(m + " ");
+    ShowNum(m + 1, n);
+}
+
+Console.Write("Введите m ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите n ");
+int n = int.Parse(Console.ReadLine());
+
+ShowNum(m,n);
