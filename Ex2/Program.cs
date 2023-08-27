4e84b45bb024d7a59fab6f77d1623dea7ee85a15
JustCheka <cheka201@yandxe.ru>
2023-08-27 22:33:04 +0300

    Ex 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumOfNum(int m, int n){
+    if (m == n) return m;
+    return m + SumOfNum(m + 1, n);
+}
+
+Console.Write("Введите m ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите n ");
+int n = int.Parse(Console.ReadLine());
+
+int k = SumOfNum(m,n);
+Console.WriteLine(k);
