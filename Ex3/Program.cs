dc27c9df27ff1c3a5214a28306e3c0536c3803cf
JustCheka <cheka201@yandxe.ru>
2023-08-27 22:46:06 +0300

    Ex 3

---
+++ Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿int Accerman(int m, int n){
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return Accerman(m -1 , 1);
+    else  return Accerman(m -1 , Accerman(m, n - 1));
+    
+}
+
+Console.Write("Введите m ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите n ");
+int n = int.Parse(Console.ReadLine());
+
+int k = Accerman(m, n);
+Console.WriteLine(k);
